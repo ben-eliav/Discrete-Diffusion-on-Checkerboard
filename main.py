74b78097a6968cc5fc5f541159ad0de35e6e5bef
ben-eliav <eliav123.spam@gmail.com>
2024-12-10 22:47:30 +0200

    none

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def main():
     args['attn'] = []
 
     for location in [args['save_weight_dir'], args['sampled_dir']]:
-        if location is None:
+        if location == "None":
             continue
         if not os.path.exists(os.path.join(location, f'Run_{args["run_id"]}')):
             os.makedirs(os.path.join(location, f'Run_{args["run_id"]}'))
